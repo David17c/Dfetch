3c76ab8d4c69732c162be54362c9120914138f64
David17c <devpelt@proton.me>
2026-05-16 20:52:05 +0200

    Updated default config file

--- customization/configfile.go
+++ customization/configfile.go
@@ -39,6 +39,7 @@ func ConfigFile() ([]string, string, error) {
 				"// Config file for Dfetch\n" +
 					"// Lines starting with '//' will be ignored\n" +
 					"// Default settings can be restored by deleting this file\n\n" +
+					"// color: blue\n" +
 					"os\n" +
 					"kernel\n" +
 					"cpu\n" +
