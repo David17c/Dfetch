a568bc2063f20768b318e6ae678d494f86d59edd
David17c <devpelt@proton.me>
2026-05-30 11:35:12 +0200

    Improved memory formatting logic

--- internal/sysinfo/memory.go
+++ internal/sysinfo/memory.go
@@ -15,33 +15,28 @@ func Memory() string {
 	}
 	defer file.Close()
 
+	var memTotal uint64
+	var memAvailable uint64
+
 	scanner := bufio.NewScanner(file)
-
-	var memTotal int
-	var memAvailable int
-
 	for scanner.Scan() {
-		line := scanner.Text()
-		fields := strings.Fields(line)
-
+		fields := strings.Fields(scanner.Text())
 		if len(fields) < 2 {
 			continue
 		}
 
 		switch fields[0] {
 		case "MemTotal:":
-			val, err := strconv.Atoi(fields[1])
+			memTotal, err = strconv.ParseUint(fields[1], 10, 64)
 			if err != nil {
 				return "unknown"
 			}
-			memTotal = val
 
 		case "MemAvailable:":
-			val, err := strconv.Atoi(fields[1])
+			memAvailable, err = strconv.ParseUint(fields[1], 10, 64)
 			if err != nil {
 				return "unknown"
 			}
-			memAvailable = val
 		}
 
 		if memTotal != 0 && memAvailable != 0 {
@@ -50,30 +45,51 @@ func Memory() string {
 	}
 
 	if err := scanner.Err(); err != nil {
-		return ""
+		return "unknown"
+	}
+
+	if memTotal == 0 || memAvailable == 0 {
+		return "unknown"
 	}
 
 	memUsed := memTotal - memAvailable
-	percentageUsed := float64(memUsed) / float64(memTotal) * 100
+	usedPercent := float64(memUsed) / float64(memTotal) * 100
+
+	const kbPerMB = 1024
+	const kbPerGB = 1024 * 1024
+	const kbPerTB = 1024 * 1024 * 1024
 
 	switch {
-	case memUsed < 1024:
-		return fmt.Sprintf("%d / %d KB (%.0f%%)", memUsed, memTotal, percentageUsed)
-
-	case memUsed < 1024*1024:
+	case memTotal >= kbPerTB:
 		return fmt.Sprintf(
-			"%.2f / %.2f MB (%.0f%%)",
-			float64(memUsed)/1024,
-			float64(memTotal)/1024,
-			percentageUsed,
+			"%.2f / %.2f TB (%.0f%%)",
+			float64(memUsed)/float64(kbPerTB),
+			float64(memTotal)/float64(kbPerTB),
+			usedPercent,
+		)
+
+	case memTotal >= kbPerGB:
+		return fmt.Sprintf(
+			"%.2f / %.2f GB (%.0f%%)",
+			float64(memUsed)/float64(kbPerGB),
+			float64(memTotal)/float64(kbPerGB),
+			usedPercent,
+		)
+
+	case memTotal >= kbPerMB:
+		return fmt.Sprintf(
+			"%.0f / %.0f MB (%.0f%%)",
+			float64(memUsed)/float64(kbPerMB),
+			float64(memTotal)/float64(kbPerMB),
+			usedPercent,
 		)
 
 	default:
 		return fmt.Sprintf(
-			"%.2f / %.2f GB (%.0f%%)",
-			float64(memUsed)/(1024*1024),
-			float64(memTotal)/(1024*1024),
-			percentageUsed,
+			"%d / %d KB (%.0f%%)",
+			memUsed,
+			memTotal,
+			usedPercent,
 		)
 	}
 }
