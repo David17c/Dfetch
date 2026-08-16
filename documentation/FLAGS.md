35620f02af2a27d3549dea2a7955aef018e89f0e
David17c <devpelt@proton.me>
2026-08-16 21:36:57 +0200

    Added shorthands for flags

--- documentation/FLAGS.md
+++ documentation/FLAGS.md
@@ -1,15 +1,15 @@
 ## Command-line flags
 
-`--help` Show available flags
+`-h, --help` Show available flags
 
-`--list-modules` List all available modules
+`-m, --list-modules` List all available modules
 
-`--no-logo` Disable ASCII art for this run
+`-l, --no-logo` Disable ASCII art for this run
 
-`--no-color` Disable color output for this run (some ASCII art may become unrecognizable)
+`-C, --no-color` Disable color output for this run (some ASCII art may become unrecognizable)
 
-`--set-logo <path / distro name>` Set the logo from a custom ASCII art file or distro name
+`-L, --set-logo <path / distro name>` Set the logo from a custom ASCII art file or distro name
 
-`--set-config <path>` Use the specified config file
+`-c, --set-config <path>` Use the specified config file
 
 `--reset-config` Regenerate the default config file
--- internal/cmd/parseflags.go
+++ internal/cmd/parseflags.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	flags "github.com/spf13/pflag"
+	"github.com/spf13/pflag"
 )
 
 type Options struct {
@@ -19,51 +19,52 @@ type Options struct {
 func ParseFlags() Options {
 	var opts Options
 
-	flags.BoolVar(&opts.NoLogo, "no-logo", false, "Disable ASCII art for this run")
-	flags.BoolVar(&opts.NoColor, "no-color", false, "Disable color output for this run")
-	flags.StringVar(&opts.SetLogo, "set-logo", "", "Set the logo from a custom ASCII art file or distro name")
-	flags.StringVar(&opts.SetConfig, "set-config", "", "Use the specified config file")
-	flags.BoolVar(&opts.ResetConfig, "reset-config", false, "Regenerate the default config file")
-	flags.BoolVar(&opts.ListModules, "list-modules", false, "List all available modules")
+	fs := pflag.NewFlagSet("myapp", pflag.ExitOnError)
 
-	flags.Parse()
+	fs.BoolVarP(&opts.NoLogo, "no-logo", "l", false, "Disable ASCII art for this run")
+	fs.BoolVarP(&opts.NoColor, "no-color", "C", false, "Disable color output for this run")
+	fs.StringVarP(&opts.SetLogo, "set-logo", "L", "", "Set the logo from a custom ASCII art file or distro name")
+	fs.StringVarP(&opts.SetConfig, "set-config", "c", "", "Use the specified config file")
+	fs.BoolVar(&opts.ResetConfig, "reset-config", false, "Regenerate the default config file")
+	fs.BoolVarP(&opts.ListModules, "list-modules", "m", false, "List all available modules")
+
+	fs.Parse(os.Args[1:])
 
 	if opts.ListModules {
-		ListAllModules()
+		listModules()
+		os.Exit(0)
 	}
 
 	return opts
 }
 
-func ListAllModules() {
+func listModules() {
 	fmt.Print(`
- Module       Description                           
+ Module       Description
  ------------ -------------------------------
- userinfo     Username and hostname                 
- os           Operating system                      
- kernel       Current kernel                        
- cpu          Processor information                 
- memory       Memory usage                          
- swap         Swap usage                            
- local_ip     Local IP address                      
- locale       Systems locale settings               
- uptime       System uptime                         
- battery      Battery information                   
- bios         BIOS information                      
- de           Desktop environment                   
- wm           Window manager                        
- shell        Current shell                         
- terminal     Current terminal                      
- disk         Disk usage                            
- time         Current time                          
- date         Current date                          
- packages     Installed packages                    
- host         Device model                          
- board        Motherboard name                      
- emptyline    Inserts a blank line                  
- text         Custom text                           
- color        Display terminals color pallet        
-
+ userinfo     Username and hostname
+ os           Operating system
+ kernel       Current kernel
+ cpu          Processor information
+ memory       Memory usage
+ swap         Swap usage
+ local_ip     Local IP address
+ locale       System locale settings
+ uptime       System uptime
+ battery      Battery information
+ bios         BIOS information
+ de           Desktop environment
+ wm           Window manager
+ shell        Current shell
+ terminal     Current terminal
+ disk         Disk usage
+ time         Current time
+ date          Current date
+ packages     Installed packages
+ host         Device model
+ board        Motherboard name
+ emptyline    Inserts a blank line
+ text         Custom text
+ color        Display terminal color palette
 `)
-	os.Exit(0)
 }
